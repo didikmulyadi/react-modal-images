463feb25e7ef39210c1e362e5facc7a7a252e814
didikmulyadi <didikmulyadi12@gmail.com>
2023-09-08 11:09:29 +0700

    feat: add example for lightbox gallery

--- src/dev.tsx
+++ src/dev.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LightBoxWithImage } from "./index";
+import { LightboxWithImage, LightboxGalleryWithImages } from "./index";
 import { createRoot } from "react-dom/client";
 
 const Demo = () => (
@@ -9,7 +9,7 @@ const Demo = () => (
     <h2>#1 with alt, small, medium and large props</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         alt="Here is the caption"
         onDownload={() => console.log("download the image")}
         fileName="Custom File Name.jpg"
@@ -23,7 +23,7 @@ const Demo = () => (
     <h2>#2 with small and large props defined only</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="assets/example_img_small.jpg"
         large="assets/example_img_large.jpg"
       />
@@ -33,7 +33,7 @@ const Demo = () => (
     <h2>#3 with small and medium props defined only</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="assets/example_img_small.jpg"
         medium="assets/example_img_medium.jpg"
       />
@@ -43,7 +43,7 @@ const Demo = () => (
     <h2>#4 with download and zoom -buttons hidden</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="assets/example_img_small.jpg"
         large="assets/example_img_large.jpg"
         hideDownload={true}
@@ -55,7 +55,7 @@ const Demo = () => (
     <h2>#5 with transparent png shown in hotpink background</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="example_transparent_heart.png"
         large="example_transparent_heart.png"
         hideDownload={true}
@@ -68,7 +68,7 @@ const Demo = () => (
     <h2>#6 with rotation -button displayed</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="assets/example_img_small.jpg"
         large="assets/example_img_large.jpg"
         showRotate={true}
@@ -79,9 +79,38 @@ const Demo = () => (
     <h2>#7 with images from external domain</h2>
 
     <div>
-      <LightBoxWithImage
+      <LightboxWithImage
         small="https://dummyimage.com/420x200/000/aaa"
         large="https://dummyimage.com/640x360/000/aaa"
+      />
+    </div>
+
+    <h2>#8 with multiple images from external domain</h2>
+
+    <div>
+      <LightboxGalleryWithImages
+        fixedWith="200px"
+        maxWidthLightBox="80%"
+        images={[
+          {
+            id: "https://dummyimage.com/420x200/000/aaa",
+            src: "https://dummyimage.com/420x200/000/aaa",
+            fileName: "image-1.jpg",
+            alt: "image-1",
+          },
+          {
+            id: "assets/example_img_large.jpg",
+            src: "assets/example_img_large.jpg",
+            srcLarge: "assets/example_img_large.jpg",
+          },
+          {
+            id: "https://cdn.glitch.com/4c9ebeb9-8b9a-4adc-ad0a-238d9ae00bb5%2Fmdn_logo-only_color.svg?1535749917189",
+            src: "https://cdn.glitch.com/4c9ebeb9-8b9a-4adc-ad0a-238d9ae00bb5%2Fmdn_logo-only_color.svg?1535749917189",
+            fileName:
+              "https://cdn.glitch.com/4c9ebeb9-8b9a-4adc-ad0a-238d9ae00bb5%2Fmdn_logo-only_color.svg?1535749917189",
+            alt: "test",
+          },
+        ]}
       />
     </div>
 
@@ -95,6 +124,9 @@ const Demo = () => (
 
 const container = document.getElementById("root");
 if (container) {
+  new EventSource("/esbuild").addEventListener("change", () =>
+    location.reload()
+  );
   const root = createRoot(container);
   root.render(<Demo />);
 }
