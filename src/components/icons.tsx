dea4fc8a74eaf8d1347318aae9d2cfe443c12db8
didikmulyadi <didikmulyadi12@gmail.com>
2023-09-08 11:07:26 +0700

    feat: add arrowback icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -81,3 +81,32 @@ export const RotateIcon = () => (
     <path d="M7.47 21.49C4.2 19.93 1.86 16.76 1.5 13H0c.51 6.16 5.66 11 11.95 11 .23 0 .44-.02.66-.03L8.8 20.15l-1.33 1.34zM12.05 0c-.23 0-.44.02-.66.04l3.81 3.81 1.33-1.33C19.8 4.07 22.14 7.24 22.5 11H24c-.51-6.16-5.66-11-11.95-11zM16 14h2V8c0-1.11-.9-2-2-2h-6v2h6v6zm-8 2V4H6v2H4v2h2v8c0 1.1.89 2 2 2h8v2h2v-2h2v-2H8z" />
   </svg>
 );
+
+export interface IArrowBackProps {
+  /**
+   * @default #000000
+   */
+  color?: string;
+  /**
+   * @default 24
+   */
+  size?: number;
+}
+
+export const ArrowBack = ({
+  color = "#000000",
+  size = 24,
+}: IArrowBackProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 24 24`}
+      height={`${size}px`}
+      width={`${size}px`}
+      fill={color}
+    >
+      <path d={`M0 0h24v24H0z`} fill="none" />
+      <path d="M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z" />
+    </svg>
+  );
+};
